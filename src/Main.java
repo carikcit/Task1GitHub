67f1408e43fac44b9e7ca744994f61987a28f561
Tahsin <carikcit@gmail.com>
2019-11-17 19:02:56 -0500

    added task description

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    // Create 3 JavaClasses: Car, Pet, Phone
+    // in three different branches: CarBranch, PetBranch, PhoneBranch
+    // add attributes to that classes at least 5 attributes
+    // add methods to that class at least 3 methods
+
+    // after creating classes merge them to master branch
+    //Main is on the master branch
+}
