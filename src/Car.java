1639877f0d1ba1d3438fd5d5782b14209098bd4a
Tahsin <carikcit@gmail.com>
2019-11-17 19:10:28 -0500

    added car with 5 attributes and 3 methods

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    public String name;
+    public int speed;
+    public String engine;
+    public int doors;
+    public String colors;
+
+    public void drive (){}
+    public  void electric(){}
+    public void isFast (){}
+    
+}
